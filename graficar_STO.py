b68c28997aeed99b0a1e56c6affb0258fbce6d1b
Rodrigo Segura <ro.sgr@ciencias.unam.mx>
2025-03-10 17:06:24 -0600

    Backup

--- graficar_STO.py
+++ graficar_STO.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def comparar_funcs(x, f, g, labels, linestyle):
+def comparar_funcs(x : np.array, f, g : list, labels : list, linestyle : list, linewidth=2):
     """ Gráfica de una función principal f(x) y múltiples funciones gi(x)
     
     x : dominio
@@ -15,7 +15,7 @@ def comparar_funcs(x, f, g, labels, linestyle):
     # Comparación función de Slater con Gaussianas
     axs[0].plot(x, f, label='Slater', color='k') # función principal
     for i in range(len(g)): # funciones aproximadas
-        axs[0].plot(x, g[i], label=labels[i], linestyle=linestyle[i])
+        axs[0].plot(x, g[i], label=labels[i], linestyle=linestyle[i], linewidth=linewidth)
     axs[0].set_xlabel('Radio (u.a.)')
     axs[0].set_ylabel(r'$R_{1s}$')
     axs[0].legend()
@@ -25,7 +25,7 @@ def comparar_funcs(x, f, g, labels, linestyle):
     k = 4*np.pi*np.power(x,2)
     axs[1].plot(x, k*np.power(f,2), label='Slater', color='k') # función principal
     for i in range(len(g)): # funciones aproximadas
-        axs[1].plot(x, k*np.power(g[i],2), label=labels[i], linestyle=linestyle[i])
+        axs[1].plot(x, k*np.power(g[i],2), label=labels[i], linestyle=linestyle[i], linewidth=linewidth)
     axs[1].set_xlabel('Radio (u.a.)')
     axs[1].set_ylabel(r'$4\pi r^2 |R_{1s}|^2$')
     axs[1].legend()
